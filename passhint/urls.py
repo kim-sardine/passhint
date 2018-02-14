dedd9fcee3a03883548578d140f86f5b72ae1c26
restforest <jhoney7374@gmail.com>
2018-02-14 22:30:09 +0900

    urls.py 버그 수정 - 순서 변경

--- passhint/urls.py
+++ passhint/urls.py
@@ -4,9 +4,11 @@ from . import views
 app_name = 'passhint'
 urlpatterns = [
     path('', views.main, name='main'),
-    path('wow/', views.wow, name='wow'),
     path('search/', views.site_search, name='site_search'),
+
     path('autocomplete/', views.autocomplete, name='autocomplete'),
 
-    path('<str:site_name>/', views.site_detail, name='site_detail')
+    path('<str:site_name>/report/', views.site_report, name='site_report'),
+    path('<str:site_name>/report-ruleset/', views.site_report_ruleset, name='site_report_ruleset'),
+    path('<str:site_name>/', views.site_detail, name='site_detail'),
 ]
