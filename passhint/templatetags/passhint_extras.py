134e32876f50f178fb6e5e93778e5b98b4ae7b0f
KIM CHUNGHUN <jhoney7374@gmail.com>
2018-02-15 19:55:11 +0900

    Rule model backup 데이터셋 생성

--- passhint/templatetags/passhint_extras.py
+++ passhint/templatetags/passhint_extras.py
@@ -33,34 +33,3 @@ def get_html_rule(rule_string):
     html = '<button type="button" class="btn btn-'+ level +' btn-sm m-1" title="'+ desc_short +'">'+ desc_en +'</button>'
 
     return mark_safe(html)
-
-
-@register.filter
-def print_password_rule(value):
-    html_list = []
-
-    if value:
-        rule_list = value.split('/')
-        for rule in rule_list:
-            rule_div = rule.split('_')
-            
-            rule_dict = deepcopy(check_dict[rule_div[0]][rule_div[1]])
-
-            if rule_div[0] == 'len':
-                rule_dict['display']['text'] = rule_dict['display']['text'].replace('{len}', rule_div[2])
-                rule_dict['description_en'] = rule_dict['description_en'].replace('{len}', rule_div[2])
-
-            html_list.append('<button type="button" class="btn btn-'+ rule_dict['display']['class'] +' btn-sm m-1" title="'+ rule_dict['description_en'] +'">'+ rule_dict['display']['text'] +'</button>')
-    return mark_safe(''.join(html_list))
-
-@register.filter
-def url_target_blank(value):
-    if value:
-        url_name = value.replace('http://', '')
-        url_name = url_name.replace('https://', '')
-        if url_name[-1] == '/':
-            url_name = url_name[:-1]
-        t_url = '<a target="_blank" href="' + value + '">' + url_name + '</a>'
-        return mark_safe(t_url)
-    else:
-        return 'no link yet'
