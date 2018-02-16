30c81a2ef975792d210b3303d2db5148f3b37d95
KIM CHUNGHUN <jhoney7374@gmail.com>
2018-02-17 00:34:46 +0900

    Report 부분 bootstrap 적용 및 이미 존재하는 Site 에 대한 Report 검사 로직 추가

--- passhint/forms.py
+++ passhint/forms.py
@@ -2,8 +2,7 @@ from django import forms
 from django.utils.translation import ugettext_lazy as _
 from django.utils.text import slugify
 
-from .models import RuleSet, ReportSite
-
+from .models import Site, RuleSet, ReportSite
 
 class SiteSearchForm(forms.Form):
     keyword = forms.CharField(
@@ -21,12 +20,29 @@ class ReportSiteForm(forms.ModelForm):
     class Meta:
         model = ReportSite
         fields = ('name', 'main_url')
-        # labels = {
-        #     "title": "Service Name"
-        #     }
-        # help_texts = {
-        #     'title': 'ex) Airasia, CGV, Github...'
-        # }
+        labels = {
+            "name": "Site Name",
+            "main_url": "Main URL"
+            }
+        help_texts = {
+            'name': 'ex) Github, Samsung, Facebook',
+            'main_url': 'ex) www.google.com, www.passhint.info'
+        }
+
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        name = cleaned_data['name']
+
+        name = slugify(name, allow_unicode=True)
+
+        if Site.objects.filter(name=name).exists():
+            raise forms.ValidationError(_("This site already exists"))
+
+        cleaned_data['name'] = name
+        return cleaned_data
+
 
 class ReportRuleSetForm(forms.ModelForm):
     class Meta:
@@ -34,15 +50,26 @@ class ReportRuleSetForm(forms.ModelForm):
         fields = ('len_min', 'len_max',
                     'exc_special','exc_space','exc_id','exc_same','exc_series','exc_id','exc_common',
                     'inc_special','inc_lower','inc_upper','inc_number','inc_letter')
-        # labels = {
-        #     "title": "Service Name"
-        #     }
-        # help_texts = {
-        #     'title': 'ex) Airasia, CGV, Github...'
-        # }
-        
+        labels = {
+            "len_min": "Minimum length of password"
+            }
+        help_texts = {
+            'len_min': 'Remain Empty if no limit'
+        }
+
+    def clean_len_min(self):
+        len_min = self.cleaned_data.get('len_min')
+        if len_min == 0:
+            len_min = None
+        return len_min
+
+    def clean_len_max(self):
+        len_max = self.cleaned_data.get('len_max')
+        if len_max == 0:
+            len_max = None
+        return len_max
+
     def clean(self):
-        
         cleaned_data = super().clean()
 
         # 하나라도 None 이 아니거나 True 라는 것은 뭐라도 했다는 거니까 Pass
