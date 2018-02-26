eecea1f392c4db916f1d431954c501ec6a7b4858
KIM CHUNGHUN <jhoney7374@gmail.com>
2018-02-26 23:52:50 +0900

    한국 사용자들에게는 한글 버전 passhint 제공 및 기타 구성 변경

--- passhint/templatetags/passhint_extras.py
+++ passhint/templatetags/passhint_extras.py
@@ -1,5 +1,7 @@
 from django import template
 from django.utils.safestring import mark_safe
+from django.utils.translation import get_language
+
 from passhint.models import Rule
 
 register = template.Library()
@@ -32,6 +34,7 @@ def get_html_rule_each(rule_string, field):
     level = rule.level
     desc_short = rule.desc_short
     desc_en = rule.desc_en
+    desc_ko = rule.desc_ko
 
     # 숫자 rule의 경우 내용 변환
     if rule_splited[0] == 'len':
@@ -39,13 +42,23 @@ def get_html_rule_each(rule_string, field):
         level = level.replace('{len}', num)
         desc_short = desc_short.replace('{len}', num)
         desc_en = desc_en.replace('{len}', num)
+        desc_ko = desc_ko.replace('{len}', num)
 
-    if field == 'desc_short':
-        text = desc_short
-    elif field == 'desc_en':
-        text = desc_en
-    else: # exception
-        text = desc_en
+
+    # display version 에 따라 형식 변경
+    language_code = get_language()
+
+    if language_code == 'ko': # 한글 사용자에게는 한글 출력
+        text = desc_ko
+    else: # 그 외 사용자에게는 영어 출력
+        if field == 'desc_short':
+            text = desc_short
+        elif field == 'desc_en':
+            text = desc_en
+        else: # exception
+            text = desc_en
+
+    # text = desc_ko
 
     html = '<button type="button" class="btn btn-'+ level +' btn-sm m-1" title="'+ desc_en +'">'+ text +'</button>'
 
